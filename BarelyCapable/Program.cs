44002b4f8ee404697dbf83a2011b1584d887e11f
Vishen Gounden <vishen.gounden@ninetyone.com>
2020-08-20 16:42:39 +0200

    put more random crap

--- BarelyCapable/Program.cs
+++ BarelyCapable/Program.cs
@@ -34,5 +34,22 @@ namespace BarelyCapable
             public List<Shape> shapes { get; set; }
         }
 
+        public class Input
+        {
+            public int Rows { get; set; }
+            public int Cols { get; set; }
+            public int ReservedSpaces { get; set; }
+            public int UniqueShapes { get; set; }
+            public List<Shape> AvailableShapes { get; set; }
+            public List<Position> ReservedSpacePositions { get; set; }
+
+        }
+
+        public class Position
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
     }
 }
