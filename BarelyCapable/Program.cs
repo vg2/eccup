4df8398515a01ba031bdf3d662d5d13279cf6193
Vishen Gounden <vishen.gounden@ninetyone.com>
2020-08-20 16:59:25 +0200

    parse input

--- BarelyCapable/Program.cs
+++ BarelyCapable/Program.cs
@@ -10,7 +10,7 @@ namespace BarelyCapable
         static void Main(string[] args)
         {
             var root = JsonConvert.DeserializeObject<Root>(File.ReadAllText("shapes_file.json"));
-
+            var input = ParseInput(File.ReadAllLines("map_1.input"), root.shapes);
             Console.WriteLine("Hello World!");
         }
 
@@ -49,6 +49,45 @@ namespace BarelyCapable
         {
             public int X { get; set; }
             public int Y { get; set; }
+        }
+
+        static Input ParseInput(string[] lines, List<Shape> shapes)
+        {
+            var input = new Input();
+            var dimensions = lines[0].Split(',');
+            input.Rows = int.Parse(dimensions[0]);
+            input.Cols = int.Parse(dimensions[1]);
+
+            var uniqueShapes = lines[1];
+            input.UniqueShapes = int.Parse(uniqueShapes);
+            var reservedCellsCount = lines[2];
+            input.ReservedSpaces = int.Parse(reservedCellsCount);
+
+            input.AvailableShapes = new List<Shape>();
+            for(var i = 3; i < 3 + input.UniqueShapes; i++)
+            {
+                var shapeEntry = lines[i].Split(',');
+                var shapeId = int.Parse(shapeEntry[0]);
+                var shapeCount = int.Parse(shapeEntry[1]);
+                
+                for (var j = 0; j < shapeCount; j++)
+                {
+                    input.AvailableShapes.Add(shapes.Find(s => s.shape_id == shapeId));
+                }
+            }
+
+            input.ReservedSpacePositions = new List<Position>();
+            var blockedCells = lines[lines.Length - 1].Split('|');
+            foreach (var blockedCell in blockedCells)
+            {
+                var xy = blockedCell.Split(',');
+                input.ReservedSpacePositions.Add(new Position {
+                    X = int.Parse(xy[0]),
+                    Y = int.Parse(xy[1])
+                });
+            }
+
+            return input;
         }
 
     }
