2f5c31a0037b5ab3bfec2a1a31bf063a4fca6bd1
Rayno Jacobs <rjacobs@silica.net>
2020-08-20 16:31:11 +0200

    stuff

---
+++ BarelyCapable/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp9
+{
+    public class Shape
+    {
+        public int shape_id { get; set; }
+        public int bounding_box { get; set; }
+        public int capacity { get; set; }
+        public List<> orientations { get; set; }
+
+    }
+}
